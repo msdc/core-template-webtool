c06cbe4e05d89d1e6eff1a09f766af4689dd99f2
wangliu <wangliu@28b60274-72dc-9e42-8378-2f9e02a2b694>
2014-12-17 06:08:29 +0000

    added pagination

--- sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
@@ -3,7 +3,7 @@
  */
 $(function(){
     //默认加载列表模板
-    loadTemplate('列表模板','template-list.html');
+    loadTemplate('列表模板','list','template-list.html');
     //注册页签事件
     registerTabPanelEvent();
 
@@ -25,44 +25,59 @@ $(function(){
  *
  * 加载主内容模板
  * @param {String} title 模板标题
+ * @param {String} contentType 内容类型
  * @param {String} templateFile 模板文件相对路径
  * */
-function loadTemplate(title,templateFile){
+function loadTemplate(title,contentType,templateFile){
     var main_content=$('#main_content');
     main_content.html('');
-    main_content.load(templateFile,function(){
-        $('#title_config').text(title);
+    switch (contentType){
+        case "list":
+        case "news":
+        {
+            main_content.load(templateFile,function(){
+                $('#title_config').text(title);
 
-        //自定义属性model
-        var customerAttrModel=function(id,target,selector,attr,filter,formater){
-            this.id=id;
-            this.target=target;
-            this.selector=selector;
-            this.attr=attr;
-            this.filter=filter;
-            this.formater=formater;
-        };
+                //自定义属性model
+                var customerAttrModel=function(id,target,selector,attr,filter,formater){
+                    this.id=id;
+                    this.target=target;
+                    this.selector=selector;
+                    this.attr=attr;
+                    this.filter=filter;
+                    this.formater=formater;
+                };
 
-        //自定义属性model集合
-        var customerAttrModelArray=[];
-        //索引器集合
-        var parseEngine=['jsoup','xpath'];
+                //自定义属性model集合
+                var customerAttrModelArray=[];
+                //索引器集合
+                var parseEngine=['jsoup','xpath'];
 
-        var customerAttrViewModel={
-            regions:ko.observableArray(customerAttrModelArray),
-            parseEngines:ko.observableArray(parseEngine),
-            /*添加解析域*/
-            addItem:function(){
-                this.regions.push(new customerAttrModel('','','','','',''));
-            },
-            getAllItems:function(){
-                //console.dir(this.regions());
-            }
-        };
+                var customerAttrViewModel={
+                    regions:ko.observableArray(customerAttrModelArray),
+                    parseEngines:ko.observableArray(parseEngine),
+                    /*添加解析域*/
+                    addItem:function(){
+                        this.regions.push(new customerAttrModel('','','','','',''));
+                    },
+                    getAllItems:function(){
+                        //console.dir(this.regions());
+                    }
+                };
 
-        //绑定到UI显示
-        ko.applyBindings(customerAttrViewModel,document.getElementById('customer_attr'));
-    });
+                //绑定到UI显示
+                ko.applyBindings(customerAttrViewModel,document.getElementById('customer_attr'));
+            });
+        }
+            break;
+        case "pagitation":
+        {
+            main_content.load(templateFile,function(){
+                $('#title_config').text(title);
+            });
+        }
+            break;
+    }
 
 }
 
@@ -73,11 +88,11 @@ function loadTemplate(title,templateFile){
 function registerTabPanelEvent(){
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         if(e.target.hash=="#list"){
-            loadTemplate('列表模板','template-list.html');
+            loadTemplate('列表模板','list','template-list.html');
         }else if(e.target.hash=="#news"){
-            loadTemplate('内容模板','template-news.html');
+            loadTemplate('内容模板','news','template-news.html');
         }else if(e.target.hash=="#pagitation"){
-            loadTemplate('分页模板','template-pagitation.html');
+            loadTemplate('分页模板','pagitation','template-pagitation.html');
         }
     });
 }
