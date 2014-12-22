af3c1ed93407485ec6711dc15885c399e87bbb6a
wangliu <wangliu@28b60274-72dc-9e42-8378-2f9e02a2b694>
2014-12-22 11:28:59 +0000

    a stable version. updated the view-model structure.

--- sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
@@ -2,13 +2,7 @@
  * Created by wang on 2014/12/9.
  */
 
-/*********************************************Models*************************************/
-/**
- *
- * 索引器集合
- * */
-var parseEngine=['jsoup','xpath'];
-
+/**************Models****************/
 /**
  *
  * 自定义属性的model
@@ -25,100 +19,27 @@ function customerAttrModel(id,target,selectorFunction,selector,attr,filter,filte
     this.formater=formater;
     this.formatCategory=formatCategory;
 }
+/**************Models****************/
 
+/**************View-Models****************/
 /**
  *
- * 模板基本信息属性对象
+ * 基本信息View-Model
  * */
-function templateBasicInfoModel(id,url,name){
-    this.id = id;
-    this.url=url;
-    this.name=name;
-    this.tags=['财经','体育','经济'];
-    this.viewHtmlContent=function(){
-        var url=this.url;
-        $('#modal-viewHtml').modal('show');
-        $.ajax({
-            url:'/webapi/crawlToolResource/viewHtmlContent',
-            type:'POST',
-            data:{
-                webUrl:url
-            },
-            success:function(result){
-                var modalBody=$('#modal-viewHtml-body');
-                modalBody.text('');//清空
-                modalBody.text(result);
-            },
-            error:function(){}
-        });
-    }.bind(this);
+function basicInfoViewModel(){
+    this.id=ko.observable('');
+    this.url=ko.observable('http://www.ccgp-shandong.gov.cn/fin_info/site/index.jsp');
+    this.name=ko.observable('山东政府采购网');
+    this.tags=ko.observableArray(['财经','体育','经济']);
 }
 
 /**
  *
- * 列表页和内容页中的单个属性实体 ,例如:标题
- * @param {String} selector
- * @param {Array} selectorAttr
- * @param {String} filter
- * @param {Array} filterCategory
- * @param {String} formater
- * @param {Array} formatCategory
+ * 自定义属性View-Model
  * */
-function commonPanelModel(selector,selectorAttr,filter,filterCategory,formater,formatCategory){
-    this.parseEngine=['jsoup','xpath'];
-    this.selector=selector;
-    this.selectorAttr=selectorAttr;
-    this.filter=filter;
-    this.filterCategory=filterCategory;
-    this.formater=formater;
-    this.formatCategory=formatCategory;
-}
-
-/**
- *
- * 分页属性model实体
- * */
-function paginationModel(selector,selectorAttr,filter,filterCategory,formater,formatCategory,paginationUrl,currentString,replaceTo,start,records){
-    this.parseEngine=['jsoup','xpath'];
-    this.selector=selector;
-    this.selectorAttr=selectorAttr;
-    this.filter=filter;
-    this.filterCategory=filterCategory;
-    this.formater=formater;
-    this.formatCategory=formatCategory;
-    this.paginationType=['PAGINATION_TYPE_PAGE','PAGINATION_TYPE_PAGENUMBER','PAGINATION_TYPE_PAGENUMBER_INTERVAL'];
-    this.paginationUrl=paginationUrl;
-    this.currentString=currentString;
-    this.replaceTo=replaceTo;
-    this.start=start;
-    this.records=records;
-}
- /*********************************************Models*************************************/
-
-/********************************************View-Models*********************************/
-
-//配置页面整个视图View-Model
-function templateViewModel(basicInfo){
-    this.basicInfo=ko.observable(basicInfo);
-    //测试模板方法
-    this.testTemplate=function(){
-        alert('testTemplate');
-    };
-    //保存模板方法
-    this.saveTemplate=function(){
-        alert('saveTemplate');
-    };
-}
-
-/**
- * 通过load方式加载的局部视图View-Model
- * 自定义属性视图中的view-model
- * @param {Array} modelArray
- * @param {Array} parseEngine
- * */
-function customerAttrViewModel(modelArray,parseEngine){
-    this.regions=ko.observableArray(modelArray);
-    this.parseEngines=ko.observableArray(parseEngine);
+function customerAttrViewModel(){
+    this.regions=ko.observableArray([]);
+    this.parseEngines=ko.observableArray(['jsoup','xpath']);
     /*添加解析域*/
     this.addItem=function(){
         this.regions.push(new customerAttrModel('','',['field','label','content','pagination'],'',['href','text','src','html'],'',['移除','匹配','替换'],'',['时间','日期']));
@@ -132,22 +53,38 @@ function customerAttrViewModel(modelArray,parseEngine){
 
 /**
  *
- * 内容页局部View-Model
+ * 分页属性View-Model
  * */
-function newsPartialViewModel(title,content){
-    this.title=ko.observable(title);
-    this.content=ko.observable(content);
+function paginationViewModel(){
+    this.parseEngine=ko.observableArray(['jsoup','xpath']);
+    this.selector=ko.observable();
+    this.selectorAttr=ko.observableArray([]);
+    this.filter=ko.observable();
+    this.filterCategory=ko.observableArray([]);
+    this.formater=ko.observable();
+    this.formatCategory=ko.observableArray([]);
+    this.paginationType=ko.observableArray(['PAGINATION_TYPE_PAGE','PAGINATION_TYPE_PAGENUMBER','PAGINATION_TYPE_PAGENUMBER_INTERVAL']);
+    this.paginationUrl=ko.observable();
+    this.currentString=ko.observable();
+    this.replaceTo=ko.observable();
+    this.start=ko.observable();
+    this.records=ko.observable();
 }
 
 /**
  *
- * 列表页局部View-Model
+ * 通用属性View-Model
  * */
-function listPartialViewModel(outlink,pagination){
-    this.outlink=outlink;
-    this.pagination=pagination;
+function commonAttrViewMode(){
+    this.parseEngine=ko.observableArray(['jsoup','xpath']);
+    this.selector=ko.observable();
+    this.selectorAttr=ko.observable(['href','text','src','html']);
+    this.filter=ko.observable();
+    this.filterCategory=ko.observable(['移除','匹配','替换']);
+    this.formater=ko.observable();
+    this.formatCategory=ko.observable(['时间','日期']);
 }
-/********************************************View-Models*********************************/
+/**************View-Models****************/
 
 /*********************************************Functions**********************************/
 $(function(){
@@ -158,11 +95,26 @@ $(function(){
         }
     };
 
-    //初始化模板内容
-    initTemplateContent();
+    $('#news_tab').load('template-news.html',function(){
+        $('#list_tab').load('template-list.html',function(){
+            //master view model with instances of both the view models.
+            var masterVM = (function(){
+                this.basicInfoViewModel = new basicInfoViewModel();
+
+                this.newsCustomerAttrViewModel=new customerAttrViewModel();
+                this.newsTitleViewModel=new commonAttrViewMode();
+                this.newsContentViewModel=new commonAttrViewMode();
+
+                this.listCustomerAttrViewModel=new customerAttrViewModel();
+                this.listOutLinkViewModel=new commonAttrViewMode();
+                this.listPaginationViewModel=new paginationViewModel();
+            })();
+            ko.applyBindings(masterVM);
+        })
+    });
 
     //模态对话框事件
-    registerModalViewContentEvent();
+    //registerModalViewContentEvent();
 });
 
 /**
@@ -175,80 +127,6 @@ function registerModalViewContentEvent(){
         modalBody.html('');//清空
         modalBody.html('<div class=\"text-center\"><img src=\"../image/load.gif\"></div>');
     })
-}
-
-/**
- *
- * 初始化页面内容
- * */
-function initTemplateContent(){
-    //模板基本信息对象
-    var basicInfo=new templateBasicInfoModel('','http://www.ccgp-shandong.gov.cn/fin_info/site/index.jsp','山东政府采购网');
-    //模板基本信息初始化
-    var pageViewModel=new templateViewModel(basicInfo);
-    ko.applyBindings(pageViewModel);
-    //列表区域内容初始化
-    loadTemplateBySelector('#list_tab','template-list.html');
-    //内容区域内容初始化
-    loadTemplateBySelector('#news_tab','template-news.html');
-};
-
-/**
- *
- * knockout.js binding the ui element.
- * @param {String} jquerySelector
- * @param {String} templateFile
- * @param {Object} attrViewModel
- * @param {Object} domElement
- * */
-function customerViewModelBinding(jquerySelector,templateFile,attrViewModel,domElement){
-    $(jquerySelector).load(templateFile,function(){
-        //绑定到UI显示
-        ko.applyBindings(attrViewModel,domElement);
-    });
-}
-
-/**
- *
- * 加载模板
- * @param {String} jquerySelector
- * @param {String} templateFile
- * */
-function loadTemplateBySelector(jquerySelector,templateFile){
-    switch (jquerySelector){
-        case "#list_tab":
-        {
-            //列表页中model集合
-            var listCustomerArray=[];
-            var listCustomerViewModel=new customerAttrViewModel(listCustomerArray,parseEngine);
-
-            var listOutLink=new commonPanelModel('body>tbody>',['href','text','src','html'],'',['移除','匹配','替换'],'YYYY-MM-DD',['时间','日期']);
-            var listPagination=new paginationModel('body>tbody>',['href','text','src','html'],'',['移除','匹配','替换'],'YYYY-MM-DD',['时间','日期'],'http://www.ccgp-fujian.gov.cn/secpag.cfm?PageNum=##&&caidan=采购公告&caidan2=公开招标&level=province&yqgg=0','##','',2,'');
-            var listPartial=new listPartialViewModel(listOutLink,listPagination);
-
-            $(jquerySelector).load(templateFile,function(){
-                ko.applyBindings(listPartial,document.getElementById('list_tab'));
-                ko.applyBindings(listCustomerViewModel,document.getElementById('list_customer_attr'));
-            });
-        }
-            break;
-        case "#news_tab":
-        {
-            //内容页中自定义属性集合
-            var newsCustomerArray=[];
-            var newsCustomerViewModel=new customerAttrViewModel(newsCustomerArray,parseEngine);
-
-            var newsTitle=new commonPanelModel('body>tbody>',['href','text','src','html'],'',['移除','匹配','替换'],'YYYY-MM-DD',['时间','日期']);
-            var newsContent=new commonPanelModel('body>tbody>',['href','text','src','html'],'',['移除','匹配','替换'],'YYYY-MM-DD',['时间','日期']);
-            var newsPartial=new newsPartialViewModel(newsTitle,newsContent);
-
-            $(jquerySelector).load(templateFile,function(){
-                ko.applyBindings(newsPartial,document.getElementById('news_tab'));
-                ko.applyBindings(newsCustomerViewModel,document.getElementById('news_customer_attr'));
-            });
-        }
-            break;
-    }
 }
 
 /**
