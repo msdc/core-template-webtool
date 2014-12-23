ebae46f7a161b4d0e3cf341f44b3c68d2de77634
wangliu <wangliu@28b60274-72dc-9e42-8378-2f9e02a2b694>
2014-12-23 10:23:54 +0000

    a stable version. added a view-model mapping in java.

---
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/webtool/CommonAttrViewModel.java
@@ -0,0 +1,26 @@
+package com.isoftstone.crawl.template.webtool;
+
+/**
+ * 
+ * 共用Model
+ * */
+public class CommonAttrViewModel {
+	private String selector="";
+	private String selectorAttr = "";
+	
+	
+	public String getSelector() {
+		return selector;
+	}
+	public void setSelector(String selector) {
+		this.selector = selector;
+	}
+	
+	
+	public String getSelectorAttr() {
+		return selectorAttr;
+	}
+	public void setSelectorAttr(String selectorAttr) {
+		this.selectorAttr = selectorAttr;
+	}
+}
---
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/webtool/ListPaginationViewModel.java
@@ -0,0 +1,88 @@
+package com.isoftstone.crawl.template.webtool;
+
+/**
+ * 
+ * 分页属性model
+ * */
+public class ListPaginationViewModel {
+	private String selector="";
+	private String selectorAttr = "";
+	private String filterCategory="";
+	private String filter = "";
+	private String paginationType="";
+	private String paginationUrl = "";
+	private String currentString="";
+	private String replaceTo = "";
+	private String start="";
+	private String records = "";
+	
+	public String getSelector() {
+		return selector;
+	}
+	public void setSelector(String selector) {
+		this.selector = selector;
+	}
+	
+	public String getSelectorAttr() {
+		return selectorAttr;
+	}
+	public void setSelectorAttr(String selectorAttr) {
+		this.selectorAttr = selectorAttr;
+	}
+	
+	public String getFilterCategory() {
+		return filterCategory;
+	}
+	public void setFilterCategory(String filterCategory) {
+		this.filterCategory = filterCategory;
+	}
+	
+	public String getFilter() {
+		return filter;
+	}
+	public void setFilter(String filter) {
+		this.filter = filter;
+	}
+	
+	public String getPaginationType() {
+		return paginationType;
+	}
+	public void setPaginationType(String paginationType) {
+		this.paginationType = paginationType;
+	}
+	
+	public String getPaginationUrl() {
+		return paginationUrl;
+	}
+	public void setPaginationUrl(String paginationUrl) {
+		this.paginationUrl = paginationUrl;
+	}
+	
+	public String getCurrentString() {
+		return currentString;
+	}
+	public void setCurrentString(String currentString) {
+		this.currentString = currentString;
+	}
+	
+	public String getReplaceTo() {
+		return replaceTo;
+	}
+	public void setReplaceTo(String replaceTo) {
+		this.replaceTo = replaceTo;
+	}
+	
+	public String getStart() {
+		return start;
+	}
+	public void setStart(String start) {
+		this.start = start;
+	}
+	
+	public String getRecords() {
+		return records;
+	}
+	public void setRecords(String records) {
+		this.records = records;
+	}
+}
