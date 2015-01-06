9aff3005f222af90bb193ad220834dcb322f3fe7
wangliu <jxbb012265@users.noreply.github.com>
2015-01-06 01:35:50 +0000

    添加模板list关联key到redis

---
+++ src/main/java/com/isoftstone/crawl/template/webtool/TemplateModel.java
@@ -0,0 +1,54 @@
+package com.isoftstone.crawl.template.webtool;
+
+/**
+ * 
+ * 模板列表实体
+ * */
+public class TemplateModel {	
+   private String templateId="";
+   private String name="";
+   private String url="";
+   private String status="";
+   private String description="";
+   private String addedTime="";
+   
+public String getTemplateId() {
+	return templateId;
+}
+public void setTemplateId(String templateId) {
+	this.templateId = templateId;
+}
+
+public String getStatus() {
+	return status;
+}
+public void setStatus(String status) {
+	this.status = status;
+}
+
+public String getDescription() {
+	return description;
+}
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String getAddedTime() {
+	return addedTime;
+}
+public void setAddedTime(String addedTime) {
+	this.addedTime = addedTime;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getUrl() {
+	return url;
+}
+public void setUrl(String url) {
+	this.url = url;
+}
+}
