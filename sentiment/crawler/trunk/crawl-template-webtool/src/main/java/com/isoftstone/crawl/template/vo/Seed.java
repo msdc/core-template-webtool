c598b3266c0d05ddbff9f8c3c39fc68d4960287b
hbdan <hbdan@28b60274-72dc-9e42-8378-2f9e02a2b694>
2015-01-21 08:01:14 +0000

    添加equals方法.

--- sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/vo/Seed.java
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/vo/Seed.java
@@ -1,34 +1,47 @@
 package com.isoftstone.crawl.template.vo;
 
 public class Seed {
-	private String url;
+    private String url;
 
-	private String isEnabled;
+    private String isEnabled;
 
-	public Seed() {
-		super();
-	}
+    public Seed() {
+        super();
+    }
 
-	public Seed(String url, String isEnabled) {
-		super();
-		this.url = url;
-		this.isEnabled = isEnabled;
-	}
+    public Seed(String url, String isEnabled) {
+        super();
+        this.url = url;
+        this.isEnabled = isEnabled;
+    }
 
-	public String getUrl() {
-		return url;
-	}
+    public String getUrl() {
+        return url;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public String getIsEnabled() {
-		return isEnabled;
-	}
+    public String getIsEnabled() {
+        return isEnabled;
+    }
 
-	public void setIsEnabled(String isEnabled) {
-		this.isEnabled = isEnabled;
-	}
+    public void setIsEnabled(String isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Seed) {
+           return ((Seed) obj).getUrl().equals(this.getUrl());
+        }
+        return false;
+    }
 
 }
