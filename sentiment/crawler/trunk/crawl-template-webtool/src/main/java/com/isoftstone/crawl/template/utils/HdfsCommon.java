fcd2b5d99ff894837159c6230a02f7374ff731c7
hbdan <hbdan@28b60274-72dc-9e42-8378-2f9e02a2b694>
2015-01-29 05:44:35 +0000

    添加hdfs支持.

--- sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/utils/HdfsCommon.java
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/java/com/isoftstone/crawl/template/utils/HdfsCommon.java
@@ -1,23 +1,39 @@
 package com.isoftstone.crawl.template.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import com.isoftstone.crawl.template.consts.WebtoolConstants;
 
 public class HdfsCommon {
 
+    /**
+     * 针对linux文件系统，将其本地文件上传到Hdfs上.
+     * @param fileName 本地文件目录.
+     * @throws IOException
+     */
     public static void upFileToHdfs(String fileName) throws IOException {
-//        // 将本地文件上传到hdfs。
-//        //"hdfs://192.168.100.231:8020/user/hdfs/tmp1/"
-//        String target = Config.getValue(WebtoolConstants.KEY_HDFS_ROOT_FOLDER);
-//        if (StringUtils.isBlank(target)) {
-//            return;
-//        }
-//        Configuration config = new Configuration();
-//        FileSystem fs = FileSystem.get(URI.create(target), config);
-//        //--拷贝全量种子文件夹.
-//        fs.copyFromLocalFile(false, true, new Path(fileName),
-//            new Path(target));
-//        // // copy
-//        // IOUtils.copyBytes(fis, os, 4096, true);
+        // 将本地文件上传到hdfs。
+        //"hdfs://192.168.100.231:8020/user/hdfs/tmp1/"
+        String target = Config.getValue(WebtoolConstants.KEY_HDFS_ROOT_FOLDER);
+        if (StringUtils.isBlank(target)) {
+            return;
+        }
+        Configuration config = new Configuration();
+        FileSystem fs = FileSystem.get(URI.create(target), config);
+        FileInputStream fis = new FileInputStream(new File(fileName));
+        OutputStream os = fs.create(new Path(target + fileName));
+        // copy
+        IOUtils.copyBytes(fis, os, 4096, true);
     }
-
+    
 }
