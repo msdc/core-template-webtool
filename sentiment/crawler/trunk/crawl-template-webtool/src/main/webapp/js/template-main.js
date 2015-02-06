1025c1e53c42f8dd1664c0ab08668800a9172f89
wangliu <wangliu@28b60274-72dc-9e42-8378-2f9e02a2b694>
2015-02-06 02:52:46 +0000

    added ajax loading method.

--- sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
+++ sentiment/crawler/trunk/crawl-template-webtool/src/main/webapp/js/template-main.js
@@ -330,7 +330,7 @@ $(function(){
     var templateGuid=getUrlParameter("templateGuid");
     //执行的是update操作
     if(templateGuid!=undefined&&templateGuid!=''&&templateGuid!=null){
-        $.ajax({
+        $.ajax2({
             url: virtualWebPath + '/webapi/crawlToolResource/updateTemplate',
             type: 'POST',
             data: {
@@ -987,3 +987,50 @@ function getUrlParameter(sParam){
         }
     }
 }
+
+/******重写Ajax操作,做成通用Loading操作*******/
+$.ajax2 = function (options, aimDiv) {
+    var img = $("<img id=\"progressImgage\"  src=\"../image/load.gif\" />"); //Loading小图标
+    var mask = $("<div id=\"maskOfProgressImage\"></div>").addClass("mask"); //Div遮罩
+    var PositionStyle = "fixed";
+    //是否将Loading固定在aimDiv中操作,否则默认为全屏Loading遮罩
+    if (aimDiv != null && aimDiv != "" && aimDiv != undefined) {
+        $(aimDiv).css("position", "relative").append(img).append(mask);
+        PositionStyle = "absolute";
+    }
+    else {
+        $("body").append(img).append(mask);
+    }
+    img.css({
+        "z-index": "2000",
+        "display": "none"
+    });
+    mask.css({
+        "position": PositionStyle,
+        "top": "0",
+        "right": "0",
+        "bottom": "0",
+        "left": "0",
+        "z-index": "1000",
+        "background-color": "#000000",
+        "display": "none"
+    });
+    var complete = options.complete;
+    options.complete = function (httpRequest, status) {
+        img.hide();
+        mask.hide();
+        if (complete) {
+            complete(httpRequest, status);
+        }
+    };
+    options.async = true;
+    img.show().css({
+        "position": PositionStyle,
+        "top": "40%",
+        "left": "50%",
+        "margin-top": function () { return -1 * img.height() / 2; },
+        "margin-left": function () { return -1 * img.width() / 2; }
+    });
+    mask.show().css("opacity", "0.1");
+    $.ajax(options);
+};
